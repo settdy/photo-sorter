c06f88563e8602c0bdd644df4a845e9cabf61395
settdy <fortestonly75@gmail.com>
2026-09-07 23:41:14 +0800

    .bat

--- Start Photo Sorter.bat
+++ Start Photo Sorter.bat
@@ -1,6 +1,8 @@
 @echo off
+title Photo Sorter
 cd /d "%~dp0"
 
+echo For more information click on https://github.com/settdy/photo-sorter/blob/main/README.md
 powershell.exe -NoProfile -ExecutionPolicy Bypass -STA -File "%~dp0PhotoSorter.ps1"
 
 if errorlevel 1 (
